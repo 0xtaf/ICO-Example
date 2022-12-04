7de948a02f23a7cb3d7f599d5b52e32361304319
Tayfun Sur <tayfunsur@gmail.com>
2022-12-04 11:37:18 +0300

    Create mint claim and withdraw functions

--- contracts/Tres.sol
+++ contracts/Tres.sol
@@ -21,6 +21,46 @@ contract Tres is ERC20, Ownable {
         ExampleNFT = IDuo(_exampleNFTContract);
     }
 
+    function mint(uint256 amount) public payable {
+        uint256 _requiredAmount = tokenPrice * amount;
+
+        require(msg.value >= _requiredAmount, "not enough balance");
+        require(
+            (totalSupply() + amount * 10**18) <= maxTokenSupply,
+            "exceeds max token supply"
+        );
+        _mint(msg.sender, amount * 10**18);
+    }
+
+    function claim() public {
+        uint256 balance = ExampleNFT.balanceOf(msg.sender);
+        require(balance > 0, "you don't have example nfts");
+
+        uint256 amount = 0;
+
+        for (uint256 i = 0; i < balance; i++) {
+            uint256 tokenId = ExampleNFT.tokenOfOwnerByIndex(msg.sender, i);
+
+            if (!tokenIdsClaimed[tokenId]) {
+                amount++;
+                tokenIdsClaimed[tokenId] = true;
+            }
+        }
+
+        require(amount > 0, "you have already claimed all the tokens");
+
+        _mint(msg.sender, amount * tokensPerNFT);
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        require(amount > 0, "contract balance is empty");
+
+        address _owner = owner();
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "failed");
+    }
+
     receive() external payable {}
 
     fallback() external payable {}
